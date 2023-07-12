30c119f3cd3b03a61b40dfe3aacb8af4070be403
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2023-07-12 16:50:22 +0600

    normal and dynamic arry and object type

--- output/script.js
+++ output/script.js
@@ -1,4 +1,47 @@
 "use strict";
-console.log("hello");
-const country = "i love bangladesh";
-console.log(country);
+// console.log("hello");
+// const country = "i love bangladesh";
+// console.log(country);
+let MyName = "pervez Hossain";
+MyName = "samira";
+let number = 34;
+console.log(MyName);
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 3));
+const fruits = ["banana", 10, [], {}];
+fruits.push({
+    name: "pervez",
+});
+console.log(fruits);
+const person = {
+    name: "Pervez",
+    age: 27,
+    isMuslim: true,
+};
+person.age = 34;
+let a;
+let b;
+a = "pervez";
+b = 23;
+console.log(a, b);
+let arr = [];
+arr.push("pervez", 20, false, {}, []);
+let c;
+c = {
+    name: "pervez",
+    age: 35,
+    adult: true,
+};
+//dynamioc type
+let d;
+(d = "pervez"), (d = 7);
+let arr2 = [];
+arr2.push("bangladesh", 34);
+let obj2;
+obj2 = {
+    name: 34,
+    age: 26,
+};
+obj2.age = 34;
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,64 @@
-console.log("hello");
-const country = "i love bangladesh";
-console.log(country);
+// console.log("hello");
+// const country = "i love bangladesh";
+// console.log(country);
+
+let MyName = "pervez Hossain";
+MyName = "samira";
+let number = 34;
+console.log(MyName);
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+console.log(sum(2, 3));
+
+const fruits = ["banana", 10, [], {}];
+fruits.push({
+  name: "pervez",
+});
+console.log(fruits);
+
+const person = {
+  name: "Pervez",
+  age: 27,
+  isMuslim: true,
+};
+person.age = 34;
+
+let a: string;
+let b: number;
+a = "pervez";
+b = 23;
+console.log(a, b);
+
+let arr: (number | string | boolean | object)[] = [];
+arr.push("pervez", 20, false, {}, []);
+
+let c: {
+  name: string;
+  age: number;
+  adult: boolean;
+};
+c = {
+  name: "pervez",
+  age: 35,
+  adult: true,
+};
+
+//dynamioc type
+
+let d: any;
+(d = "pervez"), (d = 7);
+
+let arr2: any[] = [];
+arr2.push("bangladesh", 34);
+
+let obj2: {
+  name: any;
+  age: number;
+};
+obj2 = {
+  name: 34,
+  age: 26,
+};
+obj2.age = 34;
